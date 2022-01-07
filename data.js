cdff12937552535c0ebc34c72fdb74877405feb4
vgandva <vgandvr@gmail.com>
2022-01-07 19:58:46 +0530

    Add All Files

---
+++ data.js
@@ -0,0 +1,81 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-kitchen-view",
+      "name": "KITCHEN VIEW",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.9360907097846347,
+          "pitch": -0.031747769574014484,
+          "title": "WALL TILES",
+          "text": "KAJARIA CERAMICS<div>CODE:MILAN HL-1</div>"
+        },
+        {
+          "yaw": 1.454304709965534,
+          "pitch": -0.08656605685959917,
+          "title": "WALL TILES",
+          "text": "KAJARIA CERAMICS<div><br></div><div>CODE:ALEXIS LIGHT DECOR</div>"
+        },
+        {
+          "yaw": 2.547751519844171,
+          "pitch": 0.061991071817733,
+          "title": "WALL PAINT",
+          "text": "ASIAN PAINT<div>CODE:0015 AP 8620 WHITE CLAY</div>"
+        },
+        {
+          "yaw": 3.0766905437804537,
+          "pitch": 0.1355397380132075,
+          "title": "DOOR LAMINATES",
+          "text": "CODE:Canyan ample 745 hg century laminates"
+        },
+        {
+          "yaw": 0.38101568849287304,
+          "pitch": -0.5497943900045481,
+          "title": "SHUTTER FINISH",
+          "text": "METAL FINISH<div>COPPER SHINY</div>"
+        },
+        {
+          "yaw": 1.754594370471203,
+          "pitch": -0.8281166219696807,
+          "title": "SHUTTER DIVIDER",
+          "text": "SHUTTER is divide by mirror to enhance aesthetic appearance&nbsp;"
+        },
+        {
+          "yaw": -0.18863213007978885,
+          "pitch": 1.2386857961111737,
+          "title": "FLOOR TILES",
+          "text": "COSMIC MARBLE FROM KAG TILES"
+        }
+      ]
+    }
+  ],
+  "name": "KITCHEN",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
